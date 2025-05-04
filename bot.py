195aa11c09eb082524dbfb5a2de588a7b3c87641
Khamis <93088762+Khmys@users.noreply.github.com>
2025-05-04 19:29:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
-from pyrogram.errors import FloodWait
-import asyncio
+import pyromod.listen
+from pyrogram import Client
+
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            "Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            plugins={"root": "plugins"},
+            workers=TG_BOT_WORKERS,
+            bot_token=TG_BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
 
     async def start(self):
-        try:
-            await super().start()
-        except FloodWait as e:
-            wait_time = e.value
-            print(f"[FloodWait] Telegram says wait {wait_time} seconds before restarting...")
-            await asyncio.sleep(wait_time)
-            return await self.start()  # Retry after waiting
-
+        await super().start()
         usr_bot_me = await self.get_me()
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
         self.username = usr_bot_me.username
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
